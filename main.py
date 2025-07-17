b21eaf97c8dd16e0576b0a77f433a782850d91f1
leblancr <rich.leblanc1@proton.me>
2025-07-17 17:06:43 -0400

    Added ObjectSerializer and main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from serializer.songs import Song
+from serializer.serializers import ObjectSerializer
+
+
+if __name__ == '__main__':
+    song = Song('123', 'Imagine', 'John Lennon')
+    serializer = ObjectSerializer()  #
+
+    try:
+        print(serializer.serialize(song, 'JSON'))
+        print(serializer.serialize(song, 'XML'))
+        print(serializer.serialize(song, 'YAML'))
+    except ValueError as e:
+        print(e, "not supported")
+
