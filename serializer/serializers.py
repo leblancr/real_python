f864b8e51684e8c4ae1344fb345d9840137bbd55
leblancr <rich.leblanc1@proton.me>
2025-07-18 09:55:07 -0400

    Added base.py

--- serializer/serializers.py
+++ serializer/serializers.py
@@ -1,39 +1,10 @@
 import json
 import xml.etree.ElementTree as et
 
-
-class JsonSerializer:
-    def __init__(self):
-        self._current_object = None
-
-    def start_object(self, object_name, object_id):
-        self._current_object = {
-            'id': object_id
-        }
-
-    def add_property(self, name, value):
-        self._current_object[name] = value
-
-    def to_str(self):
-        return json.dumps(self._current_object)
+from serializer.base import JsonSerializer, XmlSerializer
 
 
-class XmlSerializer:
-    def __init__(self):
-        self._element = None
-
-    def start_object(self, object_name, object_id):
-        self._element = et.Element(object_name, attrib={'id': object_id})
-
-    def add_property(self, name, value):
-        prop = et.SubElement(self._element, name)
-        prop.text = value
-
-    def to_str(self):
-        return et.tostring(self._element, encoding='unicode')
-
-
-#
+# gets a serializer object based on format type then passes it to serializable
 class ObjectSerializer:
     def serialize(self, serializable, output_format):
         serializer = factory.get_serializer(output_format)  # retrieve the serializer from the object factory
@@ -41,7 +12,7 @@ class ObjectSerializer:
         return serializer.to_str()
 
 
-# the object factory
+# the object factory, registers new format types and returns serializer objects
 class SerializerFactory:
     def __init__(self):
         self._creators = {}
@@ -52,13 +23,11 @@ class SerializerFactory:
 
     # return a serializer object based on format
     def get_serializer(self, output_format):
-        creator = self._creators.get(output_format)
+        creator = self._creators.get(output_format)  # just a class name, no parenthesis
         if not creator:
-
             raise ValueError(output_format)
-        return creator()
+        return creator()  # instantiate object here, notice parenthesis
 
 factory = SerializerFactory()
 factory.register_format('JSON', JsonSerializer)
 factory.register_format('XML', XmlSerializer)
-factory.register_format('YAML', XmlSerializer)
--- serializer/yaml_serializer.py
+++ serializer/yaml_serializer.py
@@ -1,11 +1,13 @@
 import yaml
 print(f"__name__ = {__name__}")
 print(f"__package__ = {__package__}")
-from serializer import serializers
 
-class YamlSerializer(serializers.JsonSerializer):
+from serializer.base import JsonSerializer
+from serializer.serializers import factory
+
+class YamlSerializer(JsonSerializer):
     def to_str(self):
         return yaml.dump(self._current_object)
 
 
-serializers.factory.register_format('YAML', YamlSerializer)  # pass in class name
+factory.register_format('YAML', YamlSerializer)
